08cdac2b12a4347e30bdc639e8b9b5a5afc38b65
Scurrra <iscurrra@gmail.com>
2025-11-19 20:01:50 +0300

    UbpeClassic class

---
+++ ubpe_cython/ubpe_classic.hpp
@@ -0,0 +1,37 @@
+#ifndef UBPE_CLASSIC_CPP
+#define UBPE_CLASSIC_CPP
+
+#include "ubpe_base.hpp"
+
+namespace ubpe {
+
+class UbpeClassic : protected UbpeBase {
+   private:
+    std::vector<std::vector<uint32_t>> pairs;
+
+   public:
+    UbpeClassic(uint32_t, uint32_t);
+    UbpeClassic(uint32_t, uint32_t, std::map<uint32_t, uint32_t>);
+    UbpeClassic(uint32_t, uint32_t, std::map<uint32_t, uint32_t>,
+                std::map<uint32_t, uint32_t>,
+                std::map<std::vector<uint32_t>, uint32_t>,
+                std::map<uint32_t, std::vector<uint32_t>>,
+                std::map<uint32_t, float>);
+    UbpeClassic(const UbpeClassic&) = default;
+    UbpeClassic(UbpeClassic&&) = default;
+    UbpeClassic& operator=(const UbpeClassic&) = default;
+    UbpeClassic& operator=(UbpeClassic&&) = default;
+    ~UbpeClassic() = default;
+
+    void fit(std::vector<std::vector<uint32_t>>, uint32_t = 50,
+             bool = true) override;
+
+    std::vector<std::pair<std::vector<uint32_t>, float>> encode(
+        std::vector<uint32_t>) const override;
+
+    std::vector<uint32_t> decode(std::vector<uint32_t>) const override;
+};
+
+}  // namespace ubpe
+
+#endif  // UBPE_CLASSIC_CPP
