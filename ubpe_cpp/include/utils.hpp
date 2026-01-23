3aa4130f044f11b460c6633ed92350f337a7982e
Scurrra <iscurrra@gmail.com>
2026-01-23 22:41:09 +0300

    std::variant wrapper to allow implicit casts

--- ubpe_cpp/include/utils.hpp
+++ ubpe_cpp/include/utils.hpp
@@ -3,6 +3,7 @@
 
 #include <functional>
 #include <utility>
+#include <variant>
 
 namespace ubpe {
 
@@ -20,6 +21,64 @@ struct PairHash {
         auto h1 = std::hash<T>{}(p.first);
         auto h2 = std::hash<T>{}(p.second);
         return (h1 << SHIFT) ^ h2;
+    }
+};
+
+template <typename... Types>
+class variant {
+   private:
+    std::variant<Types...> inner;
+
+   public:
+    variant() = default;
+    ~variant() = default;
+
+    variant(std::variant<Types...> data) : inner(data) {}
+
+    variant(const variant&) = default;
+    template <typename T>
+    variant(const T& data) {
+        static_assert(
+            (std::is_same_v<T, Types> || ...),
+            "`T` is not convertible to type `ubpe::variant<Types...>`");
+        this->inner = data;
+    }
+
+    variant(variant&&) = default;
+    template <typename T>
+    variant(T&& data) {
+        static_assert(
+            (std::is_same_v<T, Types> || ...),
+            "`T` is not convertible to type `ubpe::variant<Types...>`");
+        this->inner = data;
+    }
+
+    variant& operator=(const variant&) = default;
+    template <typename T>
+    variant& operator=(const T& data) {
+        static_assert(
+            (std::is_same_v<T, Types> || ...),
+            "`T` is not convertible to type `ubpe::variant<Types...>`");
+        this->inner = data;
+        return *this;
+    }
+
+    variant& operator=(variant&&) = default;
+    template <typename T>
+    variant& operator=(T&& data) {
+        static_assert(
+            (std::is_same_v<T, Types> || ...),
+            "`T` is not convertible to type `ubpe::variant<Types...>`");
+        this->inner = data;
+        return *this;
+    }
+
+    template <typename T>
+    operator T() const {
+        static_assert(
+            (std::is_same_v<T, Types> || ...),
+            "`ubpe::variant<Types...>` is not convertible to type `T`");
+        return std::get<T>(this->inner);
     }
 };
 
