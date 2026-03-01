78c877d86d2dc342eff37b4dcc25749dcad0c1f2
Scurrra <iscurrra@gmail.com>
2026-03-01 16:25:30 +0300

    enum class wrapper for operations + flags + some docs

--- ubpe_cpp/include/utils.hpp
+++ ubpe_cpp/include/utils.hpp
@@ -2,19 +2,29 @@
 #define UBPE_UTILS
 
 #include <functional>
+#include <string>
 #include <utility>
 #include <variant>
 
 namespace ubpe {
 
+/// Concept for a type that can be used as a document.
+template <typename T>
+concept DocumentT = std::ranges::range<T> ||
+                    std::is_same_v<std::remove_cvref_t<T>,
+                                   std::basic_string<typename T::value_type>>;
+
+/// Hashable concept.
 template <typename T>
 concept Hashable = requires(T a) {
     { std::hash<T>{}(a) } -> std::convertible_to<std::size_t>;
 };
 
+/// HalfSizeT concept to check if a type is half the size of a `size_t`.
 template <typename T>
 concept HalfSizeT = std::integral<T> && sizeof(T) * 2 <= sizeof(size_t);
 
+/// Hash function for pairs of HalfSizeT types.
 template <HalfSizeT T, std::size_t SHIFT = sizeof(T)>
 struct PairHash {
     std::size_t operator()(const std::pair<T, T>& p) const {
@@ -24,6 +34,15 @@ struct PairHash {
     }
 };
 
+/// `std::variant` wrapper without the need for using `std::get` to access
+/// contained data.
+///
+/// Note: This class was designed to mimic the Pythons syntax: with it one can
+/// return data of various types from functions (determined by some flag
+/// argument or smth) without the need to use `std::get` for unpacking returned
+/// data. The trick is that the deduced type is `ubpe::variant<Types...>`, but
+/// if one provides the needed type as a type of a variable or with
+/// `static_cast` the data is unpacked automatically.
 template <typename... Types>
 class variant {
    private:
@@ -79,6 +98,82 @@ class variant {
             (std::is_same_v<T, Types> || ...),
             "`ubpe::variant<Types...>` is not convertible to type `T`");
         return std::get<T>(this->inner);
+    }
+};
+
+/// `enum class` wrapper that extends the enum with `|` and `&` operators and
+/// `.has` method.
+template <typename Enum>
+    requires std::is_enum_v<Enum>
+class EnumWrapper {
+    using UnderlyingType = std::underlying_type_t<Enum>;
+    UnderlyingType value_{0};
+
+   public:
+    constexpr EnumWrapper() = default;
+
+    /// Construct a wrapper from the value of the underlying `Enum`.
+    constexpr EnumWrapper(Enum e) : value_(static_cast<UnderlyingType>(e)) {}
+
+    constexpr EnumWrapper& operator|=(const EnumWrapper& other) {
+        this->value_ |= other.value_;
+        return *this;
+    }
+
+    constexpr EnumWrapper& operator&=(const EnumWrapper& other) {
+        this->value_ &= other.value_;
+        return *this;
+    }
+
+    constexpr EnumWrapper operator|(const EnumWrapper& other) const {
+        return EnumWrapper(*this) |= other;
+    }
+
+    constexpr EnumWrapper operator&(const EnumWrapper& other) const {
+        return EnumWrapper(*this) &= other;
+    }
+
+    constexpr explicit operator bool() const { return value_ != 0; }
+    constexpr explicit operator Enum() const {
+        return static_cast<Enum>(value_);
+    }
+
+    constexpr UnderlyingType get_value() const { return this->value_; }
+
+    /// Check including of an instance of type `Enum`.
+    constexpr bool has(Enum e) const {
+        return (*this & EnumWrapper(e)).value_ != 0;
+    }
+
+    /// Check including of an instance of a type derived from this wrapper.
+    template <typename T>
+        requires std::is_base_of_v<EnumWrapper<Enum>, std::decay_t<T>>
+    constexpr bool has(const T& flag) const {
+        return (*this & flag).value_ != 0;
+    }
+};
+
+/// A wrapper that converts the passed `enum class` into a flag type that
+/// supports `|` and `&` operations, `.has` check, converting from underlying
+/// enum and creating new flags by combining multiple elements of the underlying
+/// enum.
+template <typename E>
+    requires std::is_enum_v<E>
+class Flags : public EnumWrapper<E> {
+   public:
+    using EnumWrapper<E>::EnumWrapper;
+
+    template <E Value>
+    static constexpr Flags value() {
+        return Flags(Value);
+    }
+
+    template <E... Values>
+        requires(sizeof...(Values) > 0)
+    static constexpr Flags combine() {
+        using Underlying = std::underlying_type_t<E>;
+        constexpr auto combined_value = (static_cast<Underlying>(Values) | ...);
+        return static_cast<E>(combined_value);
     }
 };
 
